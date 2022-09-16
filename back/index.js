3db6c7f800c57031c03d471dfbf174ad2bbb7b60
tocteman <juanfrancisco.vinueza@gmail.com>
2022-09-16 03:17:21 -0500

    single csv parsing

--- back/index.js
+++ back/index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const fetch = require('node-fetch')
+
 
 const app = express()
 
@@ -6,9 +8,24 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/files/data', (req, res) => {
-  res.send('all files')
+app.get('/files/data', async (req, res) => {
+  const file = await getSingleFileData()
+  res.send({ file })
 })
+
+async function getSingleFileData ()  {
+  const init = {
+    method: "GET",
+    headers: {
+      Authorization: 'Bearer aSuperSecretKey',
+      'content-type': 'text/csv;charset=UTF-8',
+    }
+  }
+  const url = 'https://echo-serv.tbxnet.com/v1/secret/file/test2.csv'
+  const res = await fetch (url, init)
+  const text = await res.text()
+  return text.split("\n").slice(1)
+}
 
 
 app.listen(3000, () => {
