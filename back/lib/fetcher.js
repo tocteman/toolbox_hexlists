bbfe8a8d2d4cef2edbb500998a5821ac4eea8731
tocteman <juanfrancisco.vinueza@gmail.com>
2022-09-18 07:46:04 -0500

    Init Functions to Objs

--- back/lib/fetcher.js
+++ back/lib/fetcher.js
@@ -5,35 +5,31 @@ const ECHO_URL = 'https://echo-serv.tbxnet.com/v1/secret'
 
 const getJson = async ({suffix}) => {
   const url = `${ECHO_URL}/${suffix}`
-  return fetch (url, initJsonGet())
+  return fetch (url, initJsonGet)
     .then(x => x.json())
     .catch(err => {console.log(err); return err})
 }
 
 const getCsv = async ({suffix}) => {
   const url = `${ECHO_URL}/${suffix}`
-  return fetch(url, initCsvGet())
+  return fetch(url, initCsvGet)
     .then(x => x.text())
     .catch(err => {console.log(err); return err})
 }
 
-const initJsonGet = () => {
-  return {
+const initJsonGet = {
     method: "GET",
     headers: {
       "authorization": `Bearer ${ECHO_KEY}`,
       'Content-Type': 'application/json',
     }
   }
-}
-const initCsvGet = () => {
-  return {
+const initCsvGet = {
     method: "GET",
     headers: {
       "Authorization": `Bearer ${ECHO_KEY}`,
       'Content-Type': 'text/csv;charset=UTF-8',
     }
   }
-}
 
 module.exports = { getJson, getCsv }
